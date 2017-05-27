b9bd2d53ce1bb967e62cab51bd93e995c8499d61
mehranbg91 <m.clash1381@gmail.com>
2017-05-27 15:22:29 +0430

    Update Client.php

--- src/Client.php
+++ src/Client.php
@@ -1,94 +1,9 @@
-<?php
-namespace Channel;
-use Workerman\Lib\Timer;
-use Workerman\Connection\AsyncTcpConnection;
-
-/**
- * Channel/Client
- * @version 1.0.3
- */
-class Client 
-{
-    /**
-     * onMessage.
-     * @var callback
-     */
-    public static $onMessage = null;
-
-    /**
-     * Connction to channel server.
-     * @var TcpConnection
-     */
-    protected static $_remoteConnection = null;
-
-    /**
-     * Channel server ip.
-     * @var string
-     */
-    protected static $_remoteIp = null;
-
-    /**
-     * Channel server port.
-     * @var int
-     */
-    protected static $_remotePort = null;
-
-    /**
-     * Reconnect timer.
-     * @var Timer
-     */
-    protected static $_reconnectTimer = null;
-    
-    /**
-     * Ping timer.
-     * @var Timer
-     */
-    protected static $_pingTimer = null;
-    
-    /**
-     * All event callback.
-     * @var array
-     */
-    protected static $_events = array();
-    
-    /**
+**
      * Ping interval.
      * @var int
      */
-    public static $pingInterval = 25;
-
-    /**
-     * Connect to channel server
-     * @param string $ip
-     * @param int $port
-     * @return void
-     */
-    public static function connect($ip = '127.0.0.1', $port = 2206)
-    {
-        if(!self::$_remoteConnection)
-        {
-             self::$_remoteIp = $ip;
-             self::$_remotePort = $port;
-             self::$_remoteConnection = new AsyncTcpConnection('frame://'.self::$_remoteIp.':'.self::$_remotePort);
-             self::$_remoteConnection->onClose = 'Channel\Client::onRemoteClose'; 
-             self::$_remoteConnection->onConnect = 'Channel\Client::onRemoteConnect';
-             self::$_remoteConnection->onMessage = 'Channel\Client::onRemoteMessage';
-             self::$_remoteConnection->connect();
-             
-             if(empty(self::$_pingTimer))
-             {
-                 self::$_pingTimer = Timer::add(self::$pingInterval, 'Channel\Client::ping');
-             }
-         }    
-    }
-    
-    /**
-     * onRemoteMessage.
-     * @param TcpConnection $connection
-     * @param string $data
-     * @throws \Exception
-     */
-    public static function onRemoteMessage($connection, $data)
+    public static $pingInte
+ 
      {
          $data = unserialize($data);
          $event = $data['channel'];
